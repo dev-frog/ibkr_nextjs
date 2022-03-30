2ac6fc420149019ce435642dafcd37eb400ce210
dev-frog <kali.nisga@gmail.com>
2022-03-30 06:06:11 +0600

    first commit

---
+++ components/Charts.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import styles from '../styles/Home.module.css';
+import * as d3 from 'd3';
+
+export default function Charts()  {
+  // create a d3 chart 
+  return (
+    <div className={styles.container}>
+      <h1>Charts</h1>
+    </div>
+  );
+}
